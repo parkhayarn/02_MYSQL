fa742d3c4b2088d732027daae75e18d7252b1e3d
parkhayarn <s0224y92@gmail.com>
2024-06-11 12:23:56 +0900

    수정

--- 00_계정생성및DB권한부여.sql
+++ 00_계정생성및DB권한부여.sql
@@ -8,18 +8,18 @@
 create user 'ohgiraffers'@'%' identified by 'ohgiraffers';
 
 -- 2) 계정이 추가된 것을 확인하기
-use mysql
+use mysql;
 select * from user;
 
 -- 데이터베이스 생성하기
---1) 데이터베이스 생성
+-- 1) 데이터베이스 생성
 create database menudb;
 -- create schema menudb; -- 이것도 같은 역할을 한다.
 
 -- MYSQL은 개념적으로 database와 스키마를 구분하지 않는다.
 SHOW databases;
 
---2) 권한 부여하기
+-- 2) 권한 부여하기
 -- menudb 데이터베이스에 대한. * (와일드카드로 모든 권한을 의미)
 -- 모든 권한을 준다. to (~에게) 어느 ip로 접속한 ohgiraffers 계정에게
 grant all privileges on menudb.* to 'ohgiraffers'@'%';

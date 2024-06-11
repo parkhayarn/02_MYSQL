49d8e0e3867e9ba994e9c24c1c98c794d51e7a3d
parkhayarn <s0224y92@gmail.com>
2024-06-11 13:05:03 +0900

    grouping

--- 07_GROUPING.sql
+++ 07_GROUPING.sql
@@ -42,3 +42,106 @@ from
 group by
     category_code;
 
+-- 2개 이상의 조건으로 그룹을 생성
+-- menu_price, category_code
+select
+    menu_price,
+    category_code,
+    count(*)
+from
+    tbl_menu
+group by
+    menu_price,
+    category_code;
+
+-- category_code, orderable_status
+select
+    category_code,
+    orderable_status,
+    count(*) 총갯수,
+    avg(menu_price) 평균,
+    sum(menu_price) 총합
+from
+    tbl_menu
+group by
+    category_code,
+    orderable_status
+order by
+    orderable_status;
+
+/*
+HAVING
+- GROUP BY 와 함께 사용되며, 그룹화된 결과에 조건을 적용할 때 사용된다.
+- WHERE 는 개별 행에 조건을 적용하는 것과 달리
+- HAVING 절은 그룹화된 데이터에 조건을 적용한다.
+*/
+
+-- menu_price, category_code로 그루핑된 데이터에서
+-- category_code가 5 이상, 8이하
+select
+    menu_price,
+    category_code
+from
+    tbl_menu
+group by
+    menu_price,
+    category_code
+having
+    category_code >= 5 and category_code <=8
+order by
+    menu_price;
+
+-- where 절로도 할 수 있다.
+-- 그룹화 전에 필터링을 할 수 없는 경우에는 HAVING으로만 가능하다.
+select
+    menu_price,
+    category_code
+from
+    tbl_menu
+where
+    category_code >=5 and category_code <=8
+group by
+    menu_price,
+    category_code
+order by
+    menu_price;
+
+-- 카테고리 코드 그룹 별  평균 가격을 구하고, 평균 가격이 8000원 이상인 경우만 조회
+select
+    category_code,
+    avg(menu_price)
+from
+    tbl_menu
+group by
+    category_code
+having
+    avg(menu_price) >= 8000;
+
+/*
+WITH ROLLUP
+- GROUP BY에서 사용되는 확장기능으로 다양한 수준의 집계 데이터를 생성 할 수 있다.
+*/
+-- 총합을 구할때
+select
+    category_code,
+    sum(menu_price)
+from
+    tbl_menu
+group by
+    category_code
+with rollup;
+
+-- 컬럼 두개를 활용한 ROLLUP
+-- 메뉴 가격별 총합 및 해당 메뉴 가격별 카테고리의 총합
+select
+    menu_price,
+    category_code,
+    count(*),
+    sum(menu_price)
+from
+    tbl_menu
+group by
+    menu_price,
+    category_code
+with rollup;
+

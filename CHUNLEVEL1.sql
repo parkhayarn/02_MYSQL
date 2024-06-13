dfac1c4d89ea282b18ac600db3139d3b39130a6e
parkhayarn <s0224y92@gmail.com>
2024-06-13 14:51:36 +0900

    춘대학 레벨1문제 연습풀이중

---
+++ CHUNLEVEL1.sql
@@ -0,0 +1,14 @@
+select * from TB_DEPARTMENT; --  학과테이블
+select * from TB_STUDENT; -- 학생테이블
+select * from TB_PROFESSOR; -- 교수테이블
+select * from TB_CLASS; -- 수업테이블
+select * from TB_CLASS_PROFESSOR; -- 수업교수테이블
+select * from TB_GRADE; -- 학점테이블
+
+
+select
+    DEPARTMENT_NAME 학과명,
+    CATEGORY 계열
+from
+    tb_department
+
